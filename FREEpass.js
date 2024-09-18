9270d055144548f2e4046fdc7b3f4ab357052e8a
merioane2020 <113840929+merioane2020@users.noreply.github.com>
2024-09-19 00:45:46 +0100

    Add files via upload

---
+++ FREEpass.js
@@ -0,0 +1,14 @@
+
+
+loopy() 
+ function loopy() { 
+ var sWord ="mrwanxX" 
+ while (sWord != "@MRWANXIOS-1DAY") {// Pass
+ sWord = prompt("𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 MRWANX IOS  ");
+
+ } 
