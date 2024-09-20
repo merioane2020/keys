0000487f0feabb76df8d32b84d0aed37cbbf7411
merioane2020 <113840929+merioane2020@users.noreply.github.com>
2024-09-20 10:03:29 +0100

    Update FREEpass.js

--- FREEpass.js
+++ FREEpass.js
@@ -3,7 +3,7 @@
 loopy() 
  function loopy() { 
  var sWord ="mrwanxX" 
- while (sWord != "@MRWANXIOS-1DAY") {// Pass
+ while (sWord != "@MRWANXIOS-30DAYMODE") {// Pass
  sWord = prompt("𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
  } 
  
