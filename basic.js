46c494f807ffd7d300d0710a5c31828d9cbce715
merioane2020 <113840929+merioane2020@users.noreply.github.com>
2024-09-21 23:20:44 +0100

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="b" 
+ while (sWord != "BASICIOS") {// Pass
+ sWord = prompt("𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 BASIC  ");
+
+ } 
