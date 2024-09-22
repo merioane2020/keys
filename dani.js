54fa1355eec32b50f955f5a96f0d203854b41fbe
merioane2020 <113840929+merioane2020@users.noreply.github.com>
2024-09-22 01:33:55 +0100

    Add files via upload

---
+++ dani.js
@@ -0,0 +1,14 @@
+
+
+loopy() 
+ function loopy() { 
+ var sWord ="mrwanxX" 
+ while (sWord != "@dani7777") {// Pass
+ sWord = prompt("𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 DANI IOS  ");
+
+ } 
